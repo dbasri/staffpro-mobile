d90f5a519c75058c307f06dbf5986ee4adce71fe
David Basri <david.basri@gmail.com>
2026-02-16 19:03:54 +0000

    Next I need to understand how to deploy the PWA to a mobile device. Firs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#F5A623" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
